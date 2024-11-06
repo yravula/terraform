8fe1ea8c3638c22c74f1c7ea9486ce830a81209a
yravula <yravula@gmail.com>
2024-11-06 12:11:32 +0530

    added tfvars

--- vpc/subnets.tf
+++ vpc/subnets.tf
@@ -3,10 +3,9 @@ availability_zone_id = var.pub_availability_zone_id
 cidr_block = var.pub_cidr_block
 vpc_id = aws_vpc.vpc-01.id
 }
-
-resource "aws_subnet" "Priv-01" {
+resource "aws_subnet" "priv-01" {
 availability_zone_id = var.priv_availability_zone_id
-cidr_block = var.priv_cidr_block"
+cidr_block = var.priv_cidr_block
 vpc_id = aws_vpc.vpc-01.id
 }
 
---
+++ vpc/variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  
+}
+variable "vpc_cidr_block" {
+  
+}
+variable "pub_availability_zone_id" {
+  
+}
+variable "pub_cidr_block" {
+  
+}
+variable "priv_availability_zone_id" {
+  
+}
+variable "priv_cidr_block" {
+  
+}
+
