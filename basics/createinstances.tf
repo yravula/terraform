688424cc730b3c2897147bb7ddb378680f2ef83f
yravula <64860246+yravula@users.noreply.github.com>
2024-11-17 14:50:24 +0000

    format

--- basics/createinstances.tf
+++ basics/createinstances.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-    region = "us-east-1"  
+  region = "us-east-1"
 }
 resource "aws_instance" "web" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t2.micro"
-  count = 3
+  count         = 3
 
   tags = {
     Name = "web-${count.idex}"
